5063d8d96cd38dc3361fcd1b73d0aca36c353b08
jacob <jacob.zschunke@artcom.de>
2015-07-29 10:51:53 +0200

    initial commit

---
+++ app/src/main/java/com/artcom/watchdog/SettingsActivity.java
@@ -0,0 +1,10 @@
+package com.artcom.watchdog;
+
+import android.preference.PreferenceActivity;
+
+public class SettingsActivity extends PreferenceActivity {
+
+    private static final String LOG_TAG = SettingsActivity.class.getSimpleName();
+
+    
+}
